d660b5de25ea894cfd900fcf34867fd733f5e635
Fina <fifinrizky50@gmail.com>
2016-11-01 11:20:45 +0700

    Intent Camera (Photo)

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,10 +1,15 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +38,35 @@ public class MainActivity extends AppCompatActivity {
                         composeSmsMessage("Pesan dari SMK Telkom Malang");
                     }
                 });
+        
+        findViewById(R.id.imageViewBrowser)
+                .setOnClickListener(new View.OnClickListener() 
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        openWebPage("http://www.smktelkom-mlg.sch.id/");
+                    }
+                });
+
+        findViewById(R.id.imageViewCamera)
+                .setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        capturePhoto();
+                    }
+                }
+                );
+    }
+
+    private void openWebPage(String url)
+    {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 
     private void composeSmsMessage(String message)
@@ -51,4 +85,26 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager())!= null)
             startActivity(intent);
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void capturePhoto()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager())!= null)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+        {
+            Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView iv = (ImageView) findViewById(R.id.imageViewCamera);
+            iv.setImageBitmap(bitmap);
+        }
+    }
+
 }
